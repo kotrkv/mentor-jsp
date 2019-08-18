c5b50f5210f872e99eb371495e9bed339ae8f8db
kostyan <kot_rkv@ukr.net>
2019-08-18 18:17:17 +0300

    Same change

--- src/main/java/com/kotrkv/mentor/jsp/filters/EncodingFilter.java
+++ src/main/java/com/kotrkv/mentor/jsp/filters/EncodingFilter.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 @WebFilter(servletNames = "addUserServlet")
 public class EncodingFilter implements Filter {
+
     private final String encodidng = "utf-8";
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -14,7 +16,6 @@ public class EncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("filter!!!!!!!!!!!!!!1");
         servletRequest.setCharacterEncoding(encodidng);
         filterChain.doFilter(servletRequest, servletResponse);
         servletResponse.setContentType("text/html;charset=UTF-8");
