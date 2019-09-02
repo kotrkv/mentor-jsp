c37723aacd52881dc56ab8547b37a0bafa67ad74
kostyan <kot_rkv@ukr.net>
2019-09-02 22:49:50 +0300

    Modify pom.xml and add static resources mapping

--- src/main/java/com/kotrkv/mentor/jsp/config/WebConfig.java
+++ src/main/java/com/kotrkv/mentor/jsp/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -20,4 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
         viewResolver.setOrder(1);
         return viewResolver;
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
+                .setCachePeriod(31556926);
+    }
+
 }
