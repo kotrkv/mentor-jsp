9664646b79c0360bff4c1cf1127f61c8b5b176eb
kostyan <kot_rkv@ukr.net>
2019-09-17 23:09:48 +0300

    First run Spring Boot

---
+++ src/main/java/com/kotrkv/mentor/jsp/config/SpringSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.kotrkv.mentor.jsp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/user").hasAnyAuthority("ADMIN", "USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .and()
+                .exceptionHandling().accessDeniedPage("/accessDenied")
+                .and()
+                .authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .permitAll()
+                .usernameParameter("login")
+                .loginProcessingUrl("/login")
+                .successForwardUrl("/auth")
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/");
+
+    }
+
+    // create two users, admin and user
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
+}
